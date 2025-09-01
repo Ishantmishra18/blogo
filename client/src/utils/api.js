e64c312402d9869342815a2adc7c1e25eb55e40c
ishantmishra18 <ishant8mishra@gmail.com>
2025-09-01 12:31:04 +0530

    basic structure

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',  //https://travo-lg0h.onrender.com/  live hosted
+  baseURL: 'http://localhost:3000/api',  //https://travo-lg0h.onrender.com/  live hosted
   withCredentials: true, // if using cookies  //http://localhost:3000/ for development
 });
 
